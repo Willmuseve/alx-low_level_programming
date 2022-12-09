266a9f37b6b8867e45262602e0c2338720226a71
Will Nah <willmuseve@gmail.com>
2022-12-08 16:42:50 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
-
 #include <stdio.h>
+/**
+ * Main does the printing
+ * Return, returns 0 since it is not an integer*/
+
 int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
