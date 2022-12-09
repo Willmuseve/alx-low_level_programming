13639af6196952845a49068c61b952d5fd97ec6a
Will Nah <willmuseve@gmail.com>
2022-12-08 17:11:04 -0800

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * Main - Prints a given phrase
+ * main - Prints a given phrase
  * Return: returns 0 if exited properl, non-zero if not
  */
-int  main(void)
+int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
