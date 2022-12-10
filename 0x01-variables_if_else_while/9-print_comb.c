7b24f2563a13e7c77c0733bd5bdba224c0d5e190
Will Nah <willmuseve@gmail.com>
2022-12-09 23:55:14 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  a program that prints all possible combinations
+ *  of single-digit numbers.
+ *  Return: 0
+ */
+int main (void)
+{
+	int will;
+
+	for (will = 0; will < 100; will++)
+	{
+		purchar(will + '0');
+		if (will < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
