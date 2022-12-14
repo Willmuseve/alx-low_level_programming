502a169adeeee1e4d2bfd465bd7bd2ce73f2c2fd
Will Nah <willmuseve@gmail.com>
2022-12-14 02:51:49 -0800

    The alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,35 +1,17 @@
-#nclude "main.h"                                                
+#include "main.h"
+/**
+ * main - checks the code
+ * Return: always 0,
+ */
+void print_alphabet(void)
+{
+	int c = 'a';
 
-/**                                                            
- *
- *  * main - check the code                                       
- *
- *   * Return: Always 0.                                           
- *
- *    */                                                            
-
-int main(void)                                                 
-
-{                                                              
-
-	        int a;                                                 
-
-		        int z;                                                 
-
-			                                                               
-
-			        while(a <= z)                                          
-
-					        {                                                      
-
-							                putchar('a');                                  
-
-									                a++;                                           
-
-											        }                                                      
-
-				        putchar('\n');                                         
-
-					        return (0);                                            
-
-} 
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
