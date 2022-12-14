12c534171f71ebf900f3b1771342dd6fd18fe1b1
Will Nah <willmuseve@gmail.com>
2022-12-14 04:09:34 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,10 +6,11 @@
  */
 int _isalpha(int c)
 {
-	if ((c < 'z' && c > 'a') || (c < 'Z' && C > 'A' ))
+	if ((c < 'z' && c > 'a') || (c < 'Z' && c > 'A' ))
 	{
 		return (1);
 	}
+
 	else 
 	{
 		return (0);
