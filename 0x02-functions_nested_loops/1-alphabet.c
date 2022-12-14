db7d4317d84a48ce4e7269b32b04ced9f30d44c0
Will Nah <willmuseve@gmail.com>
2022-12-14 02:00:56 -0800

    Alpabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#nclude "main.h"                                                
+
+/**                                                            
+ *
+ *  * main - check the code                                       
+ *
+ *   * Return: Always 0.                                           
+ *
+ *    */                                                            
+
+int main(void)                                                 
+
+{                                                              
+
+	        int a;                                                 
+
+		        int z;                                                 
+
+			                                                               
+
+			        while(a <= z)                                          
+
+					        {                                                      
+
+							                putchar('a');                                  
+
+									                a++;                                           
+
+											        }                                                      
+
+				        putchar('\n');                                         
+
+					        return (0);                                            
+
+} 
