73d00fed9a7b7524ef8d2cea502d8a60ddb1e9b5
Will Nah <willmuseve@gmail.com>
2022-12-14 04:43:28 -0800

    Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * _abs - starting point 
- * Return: always 0 
+ * _abs - starting point
+ * @r: has value
+ * Return: always 0
  */
 int _abs(int r)
 {
 	if (r < 0)
 	{
-		return (r *(-1));
+		return (r * (-1));
 	}
 	else if (r == 0)
 	{
