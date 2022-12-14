284484e02cbb084c7f8dc2c43b18656dd8d3ac09
Will Nah <willmuseve@gmail.com>
2022-12-14 01:38:42 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include "main.h"                                                
+#include "main.h"
 /*
  * main - prints putchar followed by a new line
  * Return: returns 0
- */ 
-int main(void)                                                 
-{                                             
+ */
+int main(void)
+{                                
 
 	 _putchar('_');
 	 _putchar('p');
@@ -15,6 +15,5 @@ int main(void)
 	 _putchar('a');
 	 _putchar('r');
 	 _putchar('\n');
-	 
-	 return (0); 
-} 
+	 return (0);
+}
