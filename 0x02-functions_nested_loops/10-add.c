64a3e2ef733b6c2506cf56321efcb2d52bbbe923
Will Nah <willmuseve@gmail.com>
2022-12-14 06:23:46 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - prints two numbers
+ * @m: print int m
+ * @w: print int w
+ * Return: always 0
+ */
+int add(int m , int w)
+{
+	return (m + w);
+}
