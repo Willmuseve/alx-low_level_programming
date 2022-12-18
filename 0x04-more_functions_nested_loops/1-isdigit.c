7c76e7e25856d45b37f3c0f99af65cbaaacb575b
Will Nah <willmuseve@gmail.com>
2022-12-18 12:24:34 -0800

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for a digit 
+ * @c: contains the value 
+ * return: returns 0
+ */
+
+int _isdigit(int c)
+{
+	for (c =0; c < 10; c++)
+	{
+		if (c < 10 && c > 10)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
