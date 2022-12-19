f79ea5d189d67123a68cb8ab4c17f25119ab9402
Will Nah <willmuseve@gmail.com>
2022-12-18 21:39:34 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int p = 100;
+
+	i = i;
+	while (i <= p)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("fizzbuzz");
+		}
+		else if(i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			if  (i < p)
+			printf("Buzz");
+
+			else
+				printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
