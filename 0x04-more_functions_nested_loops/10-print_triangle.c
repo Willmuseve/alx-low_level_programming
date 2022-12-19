3a2bac6ebf3e58ae1e82931675c5ea497786c9fb
Will Nah <willmuseve@gmail.com>
2022-12-18 22:08:17 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle -prints the triangle
+ * @size  - contains value
+ * Return: returns 0
+ */
+void print_triangle(int size)
+{
+	int i = 1, x;
+
+	while (i <= size && size > 0)
+	{
+		x = 0;
+		while (x < size - i)
+		{
+			_putchar(' ');
+			x++;
+		}
+		x = 0;
+		while (x < i)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
+}
