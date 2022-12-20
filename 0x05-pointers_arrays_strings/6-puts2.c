312aee24181e74d5176ce19c3ec7781e8017c155
Will Nah <willmuseve@gmail.com>
2022-12-20 09:23:58 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+void puts2(char *str)
+{
+	int c = 0;
+
+	while (c >= 0)
+	{
+		if (str[c] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		if (c % 2 == 0)
+			putchar(str[c]);
+		c++;
+	}
+}
