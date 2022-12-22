cd79a2afef95e5649a86acc37fbf42b6be9bcfe5
Will Nah <willmuseve@gmail.com>
2022-12-22 02:09:03 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
  * _strncpy - Copies a string
  * @dest: The buffer storing the string copy
  * @src: The source string
- * @n: The maximum number of bytes to copied from src. 
+ * @n: The maximum number of bytes to copied from src.
  * Return: A pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
