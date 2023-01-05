ab2f8134837c04a1bc38585c80707a3a01ce5ec3
Will Nah <willmuseve@gmail.com>
2023-01-05 11:35:03 -0800

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * length - Returns the length of a string.
+ * @s: The string to be measured.
+ * Return: The length of the string.
+ */
+int length(char *s);
+int check_palindrome(char *s, int len, int index);
+int check_palindrome(char *s, int len, int index);
+
+int length(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+	return (len);
+}
