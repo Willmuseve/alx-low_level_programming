cb64e9ae1cfe57533b25cbe909793f549242727e
Will Nah <willmuseve@gmail.com>
2023-01-17 04:57:00 -0800

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,13 +3,13 @@
 
 
 	/**
-	 * struct dog - basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
+	* struct dog - basic info
+	* @name: First member
+	* @age: Second member
+	* @owner: Third member
+	*
+	* Description: Longer description
+	*/
 	struct dog
 	{
 		char *name;
@@ -19,8 +19,9 @@
 
 
 	/**
-	 * dog_t - typedef for struct dog
-	 */
+	* dog_t - typedef for struct dog
+	* struct dog - basic info
+	*/
 	typedef struct dog dog_t;
 
 	void init_dog(struct dog *d, char *name, float age, char *owner);
