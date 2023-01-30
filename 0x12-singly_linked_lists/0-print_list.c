caec1da0a0071e7735f0f596ed3149fe2af80da1
willmuseve <willmuseve@gmail.com>
2023-01-30 06:23:11 -0800

    printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,14 +13,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
 		s++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 
 	return (s);
 }
-
