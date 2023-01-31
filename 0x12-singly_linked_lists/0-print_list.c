fd28be31a2e3cb53c90f0b28dd28559d532b2f94
willmuseve <willmuseve@gmail.com>
2023-01-30 23:00:45 -0800

    printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- *
- * Return: the number of nodes printed
+ * print_list - prints list.
+ * @h: param list
+ * Return: struct
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t l_node = 0;
