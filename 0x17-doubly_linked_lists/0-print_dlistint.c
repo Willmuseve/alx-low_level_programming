6a091c26d8f8662fea7c7b60895b31748c2a9dac
will museve <willmuseve@gmail.com>
2023-03-23 23:12:47 -0700

    print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints length and elements
+ * @h: head of list
+ * Return: size_t value
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *t;
+	int count = 0;
+
+	t = h;
+	while (t != NULL)
+	{
+		printf("%d\n", t->n);
+		t = t->next;
+		count++;
+	}
+	return (count);
+}
