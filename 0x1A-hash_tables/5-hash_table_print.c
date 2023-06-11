fd9b2ebee30f424c6c36e319ef16d9aa61c6c6b4
Willmuseve <willmuseve@gmail.com>
2023-06-11 01:08:40 -0700

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Prrint a hash table.
+ * @ht: A pointer to the hash table to be printed.
+ * Description: Key/value pairs are printed in the order
+ * they appear in the array of the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int j;
+	unsigned char k = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (j = 0; j < ht->size; j++)
+	{
+		if (ht->array[j] != NULL)
+		{
+			if (k == 1)
+				printf(", ");
+
+			node = ht->array[j];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			k = 1;
+		}
+	}
+	printf("}\n");
+}
