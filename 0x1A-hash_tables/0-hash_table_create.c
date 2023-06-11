ae0071be947f14086e3670492819a10e3d7f84f6
Willmuseve <willmuseve@gmail.com>
2023-06-11 00:12:54 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,24 +8,28 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table_created;
-	unsigned long int j;
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-	hash_table_created = malloc(sizeof(hash_table_t));
-
-	if (hash_table_created == NULL)
+	if (size == 0)
 		return (NULL);
 
-	hash_table_created->size = size;
-	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+	hash_table = malloc(sizeof(hash_table_t));
 
-	if (hash_table_created->array == NULL)
+	if (hash_table == NULL)
 		return (NULL);
 
-	for (j = 0; j < size; j++)
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (hash_table->array == NULL)
 	{
-		hash_table_created->array[j] = NULL;
+		free(hash_table);
+		return (NULL);
 	}
 
-	return (hash_table_created);
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+
+	return (hash_table);
 }
