a2639ffc9127e27f242a815c4943db3ab0ee34d0
Willmuseve <willmuseve@gmail.com>
2023-06-11 00:33:35 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,9 @@
 #include "hash_tables.h"
-
 /**
  *hash_table_create- A function that creates a hash table
- *@size: Arguments of the table
+ *@size: size of the table
  *Return: A pointer or NULL
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newT;
